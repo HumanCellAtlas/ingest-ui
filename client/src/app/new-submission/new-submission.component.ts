de2f19f6421e6c3f36a931428fa99681c82922d4
Daniel Vaughan <dvaughan@ebi.ac.uk>
2017-11-15 15:56:43 +0000

    added bootstrap

---
+++ client/src/app/new-submission/new-submission.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-new-submission',
+  templateUrl: './new-submission.component.html',
+  styleUrls: ['./new-submission.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NewSubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
