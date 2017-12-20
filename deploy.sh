a691b4357c38f960e1d6e40aa0521e8a1b52480a
Daniel Vaughan <dvaughan@ebi.ac.uk>
2017-12-20 13:52:20 +0000

    Added CloudFront invalidation to the script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+# Build the client
 cd client
 npm install
 ng build --prod
+# Build and run the install script
 cd ..
 npm install
 serverless client deploy -v
+# Configure CloudFront support in aws-cli
+aws configure set preview.cloudfront true
+# Invalidate CloudFront cache
+aws cloudfront create-invalidation --distribution-id E1GC8ZIY6A9571 --paths /*
