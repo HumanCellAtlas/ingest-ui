2857e544c9ef25a2357099818c695490c7ffba94
Alegria Aclan <aaclan@ebi.ac.uk>
2018-02-28 10:44:25 +0000

    add integration environment configuration for deployment

---
+++ client/src/environments/environment.integration.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  buildTimestamp: '',
+  INGEST_API_URL: 'http://api.ingest.integration.data.humancellatlas.org',
+  BROKER_API_URL: 'http://ingest.integration.data.humancellatlas.org',
+  SCHEMA_API_URL: 'https://schema.humancellatlas.org'
+};
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+
+# To deploy, run source deploy.sh <dev|integration|staging|prod>
+# The auth0 config file ./client/src/app/auth/auth0-variables.ts must also be setup
+
 # Build the client
 ENV="$1"
 DIST_ID_DEV="E1GC8ZIY6A9571"
+DIST_ID_INTEGRATION="E3G3XIA004X2WL"
 DIST_ID_STAGING="E2D5GGR57ZBWOQ"
 
 if [ "$ENV" == 'prod' ]; then
     CLOUDFRONT_ID=${DIST_ID_STAGING}
 elif [ "$ENV" == 'staging' ]; then
+    CLOUDFRONT_ID=${DIST_ID_INTEGRATION}
+elif [ "$ENV" == 'integration' ]; then
     CLOUDFRONT_ID=${DIST_ID_STAGING}
 else
     ENV='dev'
