4d20ffc4bf02103864ba77e316a47921891b14e9
Alegria Aclan <aaclan@ebi.ac.uk>
2018-06-11 10:39:48 +0100

    User must be redirected to login page when session expires

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {AuthService} from './auth/auth.service';
 import {LoaderService} from "./shared/services/loader.service";
 import {SchemaService} from "./shared/services/schema.service";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
 
 @Component({
   selector: "app-root",
@@ -10,12 +11,21 @@ import {SchemaService} from "./shared/services/schema.service";
 })
 export class AppComponent {
   showLoader: boolean;
+  authInterval = 5000;
+  alive = true;
 
   constructor(public auth: AuthService,  private loaderService: LoaderService, private schemaService: SchemaService) {
-    auth.handleAuthentication();
+    TimerObservable.create(0, this.authInterval)
+      .takeWhile(() => this.alive) // only fires when component is alive
+      .subscribe(() => {
+         auth.handleAuthentication();
+      });
+
     this.loaderService.status.subscribe((val: boolean) => {
       this.showLoader = val;
     });
+
+
 
   }
 
