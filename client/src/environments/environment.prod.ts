babf8a5509eaf58e7ca88c846de8a79ffe22337c
Alegria Aclan <aaclan@ebi.ac.uk>
2018-07-18 15:35:32 +0100

    Added DSS API url in prod in config

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   buildTimestamp: '',
   INGEST_API_URL: 'http://api.ingest.data.humancellatlas.org',
   BROKER_API_URL: 'http://ingest.data.humancellatlas.org',
-  SCHEMA_API_URL: 'https://schema.humancellatlas.org'
+  SCHEMA_API_URL: 'https://schema.humancellatlas.org',
+  DSS_API_URL: 'https://dss.data.humancellatlas.org'
 };
