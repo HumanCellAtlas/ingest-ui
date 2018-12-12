227e07e32c49d6b845b1b4d3301d2cf24e7b7d4c
Alegria Aclan <aaclan@ebi.ac.uk>
2018-12-12 14:30:37 +0000

    Added auth0 config in env config for prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -5,5 +5,11 @@ export const environment = {
   INGEST_API_URL: 'http://api.ingest.data.humancellatlas.org',
   BROKER_API_URL: 'http://ingest.data.humancellatlas.org',
   SCHEMA_API_URL: 'https://schema.humancellatlas.org',
-  DSS_API_URL: 'https://dss.data.humancellatlas.org'
+  DSS_API_URL: 'https://dss.data.humancellatlas.org',
+  auth0: {
+    clientID: 'TISXO9tlrj5wt6xwEkq8tM3mCpzdRABE',
+    domain: 'danielvaughan.eu.auth0.com',
+    callbackURL: 'http://ui.ingest.data.humancellatlas.org/callback',
+    apiUrl: 'http://localhost:8080'
+  }
 };
