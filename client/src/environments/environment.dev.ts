5a9bf6b927b5e2cf8b0ce812b1b765d075fa89fa
Alegria Aclan <aaclan@ebi.ac.uk>
2018-12-18 15:24:49 +0000

    Remove auth0 variables

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -6,4 +6,5 @@ export const environment = {
   BROKER_API_URL: 'http://ingest.dev.data.humancellatlas.org',
   SCHEMA_API_URL: 'https://schema.humancellatlas.org',
   DSS_API_URL: 'https://dss.dev.data.humancellatlas.org'
+
 };
