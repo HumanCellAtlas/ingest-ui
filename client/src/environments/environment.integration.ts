0eaeb926cf72aedafe114f76d152847eda04b18e
Alegria Aclan <aaclan@ebi.ac.uk>
2019-01-21 15:29:08 +0000

    Point to ingest api and broker api https uri

--- client/src/environments/environment.integration.ts
+++ client/src/environments/environment.integration.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   buildTimestamp: '',
   commitHash: '',
-  INGEST_API_URL: 'http://api.ingest.integration.data.humancellatlas.org',
-  BROKER_API_URL: 'http://ingest.integration.data.humancellatlas.org',
+  INGEST_API_URL: 'https://api.ingest.integration.data.humancellatlas.org',
+  BROKER_API_URL: 'https://ingest.integration.data.humancellatlas.org',
   SCHEMA_API_URL: 'https://schema.humancellatlas.org',
   DSS_API_URL: 'https://dss.integration.data.humancellatlas.org'
 };
