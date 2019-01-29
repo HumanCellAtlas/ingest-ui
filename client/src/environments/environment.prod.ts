38167ba0d774fa3a1467f6d5a7ce2116da5941c0
Alegria Aclan <aaclan@ebi.ac.uk>
2019-01-29 16:17:41 +0000

    Point to ingest https

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   buildTimestamp: '',
   commitHash: '',
-  INGEST_API_URL: 'http://api.ingest.data.humancellatlas.org',
-  BROKER_API_URL: 'http://ingest.data.humancellatlas.org',
+  INGEST_API_URL: 'https://api.ingest.data.humancellatlas.org',
+  BROKER_API_URL: 'https://ingest.data.humancellatlas.org',
   SCHEMA_API_URL: 'https://schema.humancellatlas.org',
   DSS_API_URL: 'https://dss.data.humancellatlas.org'
 };
