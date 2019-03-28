11de3c4f6bbf8006daba3a359090fd0e819eaa2e
Alegria Aclan <aaclan@ebi.ac.uk>
2019-03-28 14:52:24 +0000

    Dockerise app

---
+++ client/src/environments/environment.env.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  buildTimestamp: '',
+  commitHash: '',
+  INGEST_API_URL: 'https://api.ingest.dev.data.humancellatlas.org',
+  BROKER_API_URL: 'https://ingest.dev.data.humancellatlas.org',
+  SCHEMA_API_URL: 'https://schema.humancellatlas.org',
+  DSS_API_URL: 'https://dss.dev.data.humancellatlas.org'
+
+};
