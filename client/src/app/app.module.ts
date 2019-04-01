43cc580083114387c37632dac5902517d2c47d3c
Alegria Aclan <aaclan@ebi.ac.uk>
2019-04-01 16:05:05 +0100

    Added ingest prod urls in the whitelist so that we can use same image for prod env

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -109,6 +109,7 @@ export function tokenGetter(): string {
         tokenGetter: tokenGetter,
         whitelistedDomains: [
           "localhost:8080", "localhost:5000",
+          "api.ingest.data.humancellatlas.org", "ingest.data.humancellatlas.org",
           "api.ingest.testing.data.humancellatlas.org", "ingest.testing.data.humancellatlas.org",
           "api.ingest.dev.data.humancellatlas.org", "ingest.dev.data.humancellatlas.org",
           "api.ingest.integration.data.humancellatlas.org", "ingest.integration.data.humancellatlas.org",
