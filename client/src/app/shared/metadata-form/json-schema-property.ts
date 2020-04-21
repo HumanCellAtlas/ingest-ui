e234efa66629a22dbe656d8f184a453a24325f6e
Alegria Aclan <aaclan@ebi.ac.uk>
2020-04-21 09:48:11 +0100

    Remove commented out fields

--- client/src/app/shared/metadata-form/json-schema-property.ts
+++ client/src/app/shared/metadata-form/json-schema-property.ts
@@ -1,9 +1,7 @@
 import {JsonSchema} from './json-schema';
 
 export interface JsonSchemaProperty extends JsonSchema {
-  // key: string;
   user_friendly: string;
   example: string;
-  // is_required_property: boolean;
   items?: object;
 }
