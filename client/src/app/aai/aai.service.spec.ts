4c4df0a2b0721c501e5c753f2c20d07f11451fa4
Rodrey Goite <rodrey@ebi.ac.uk>
2020-04-23 11:47:40 +0100

    Setup specs for completeAuthentication.

---
+++ client/src/app/aai/aai.service.spec.ts
@@ -0,0 +1,8 @@
+describe('Complete Authentication', () =>{
+
+  it('should redirect home when the user is registered');
+
+  it('should redirect to registration page when User has not registered yet');
+
+});
+
