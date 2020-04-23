ca543868b4356e6a8512c0a5ffb69dbe17c8ce85
Rodrey Goite <rodrey@ebi.ac.uk>
2020-04-24 00:00:11 +0100

    Defined CoreSecurity module.

--- client/src/app/core/authentication.service.ts
+++ client/src/app/core/authentication.service.ts
@@ -2,9 +2,10 @@ import {Observable} from "rxjs";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
+import {CoreSecurity} from "./security.module";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: CoreSecurity,
 })
 export class AuthenticationService {
 
