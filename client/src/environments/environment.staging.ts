a23ae2e585df60bae4c9455e95efab8cb35e8d84
Alegria Aclan <aaclan@ebi.ac.uk>
2020-05-20 10:31:31 +0100

    Updated environment urls

--- client/src/environments/environment.staging.ts
+++ client/src/environments/environment.staging.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  INGEST_API_URL: 'https://api.ingest.staging.data.humancellatlas.org',
-  BROKER_API_URL: 'https://ingest.staging.data.humancellatlas.org',
+  INGEST_API_URL: 'https://api.ingest.staging.archive.data.humancellatlas.org',
+  BROKER_API_URL: 'https://ingest.staging.archive.data.humancellatlas.org',
   SCHEMA_API_URL: 'https://schema.humancellatlas.org',
   DSS_API_URL: 'https://dss.staging.data.humancellatlas.org',
-  DOMAIN_WHITELIST: 'api.ingest.staging.data.humancellatlas.org,ingest.staging.data.humancellatlas.org',
+  DOMAIN_WHITELIST: 'api.ingest.staging.archive.data.humancellatlas.org,ingest.staging.archive.data.humancellatlas.org',
   // AAI
   AAI_CLIENT_ID: '',
   AAI_AUTHORITY: 'https://login.elixir-czech.org/oidc',
