f1ead136f32f9a22da5cf718f27bf08130255f27
Alegria Aclan <aaclan@ebi.ac.uk>
2020-06-30 14:44:06 +0100

    Added publication url field

--- client/src/app/project-registration/publication-field-group/publication-field-group.component.ts
+++ client/src/app/project-registration/publication-field-group/publication-field-group.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Metadata} from '../../metadata-schema-form/models/metadata';
+import {AbstractControl} from '@angular/forms';
+import {MetadataForm} from '../../metadata-schema-form/models/metadata-form';
 
 @Component({
   selector: 'app-publication-field-group',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publication-field-group.component.css']
 })
 export class PublicationFieldGroupComponent implements OnInit {
+  @Input()
+  metadataForm: MetadataForm;
+
+  publicationUrlMetadata: Metadata;
+  publicationUrlControl: AbstractControl;
+  publicationUrlId: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.publicationUrlMetadata = this.metadataForm.get('project.content.publications.url')
+    const publicationsControl = this.metadataForm.getControl('project.content.publications');
+
+    this.publicationUrlControl = publicationsControl['controls'][0]['controls']['url'];
+
+    this.publicationUrlId = 'project.content.publications.url';
   }
+
 
 }
