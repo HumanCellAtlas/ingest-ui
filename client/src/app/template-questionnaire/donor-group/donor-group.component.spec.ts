1ce9437f22e665912d3b947a8e3ac9423c679af8
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2020-07-20 16:13:09 +0100

    donor group fixup

--- client/src/app/template-questionnaire/donor-group/donor-group.component.spec.ts
+++ client/src/app/template-questionnaire/donor-group/donor-group.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DonorGroupComponent } from './donor-group.component';
 
-describe('SpecimenGroupComponent', () => {
+describe('DonorGroupComponent', () => {
   let component: DonorGroupComponent;
   let fixture: ComponentFixture<DonorGroupComponent>;
 
