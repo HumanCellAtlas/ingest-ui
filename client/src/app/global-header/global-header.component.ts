4cf7b38a8a1dda869901fe135eb5b49bfeb15b88
Alegria Aclan <aaclan@ebi.ac.uk>
2020-07-27 20:39:31 +0100

    Do not display the navigation links or user name if account is not found

--- client/src/app/global-header/global-header.component.ts
+++ client/src/app/global-header/global-header.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {Profile} from 'oidc-client';
 import {Router} from '@angular/router';
 import {AaiService} from '../aai/aai.service';
+import {concatMap, map} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {Account} from '../core/account';
+import {IngestService} from '../shared/services/ingest.service';
 
 @Component({
   selector: 'app-global-header',
@@ -11,21 +15,31 @@ import {AaiService} from '../aai/aai.service';
 export class GlobalHeaderComponent implements OnInit {
   isLoggedIn: boolean;
   userInfo: Profile;
+  account$: Observable<Account>;
+  isLoggedIn$: Observable<boolean>;
 
-  constructor(private router: Router,
-              private aai: AaiService) {
-    this.aai.user$.subscribe(user => {
-      this.isLoggedIn = user && !user.expired;
-      this.userInfo = user ? user.profile : null;
-    });
+  constructor(private router: Router, private ingestService: IngestService, private aai: AaiService) {
+  }
+
+  ngOnInit() {
+    this.isLoggedIn$ = this.aai.isUserLoggedIn();
+    this.account$ = this.aai.user$.pipe(
+      map(user => {
+        this.isLoggedIn = user && !user.expired;
+        this.userInfo = user ? user.profile : null;
+        return this.isLoggedIn;
+      }),
+      concatMap(loggedIn => {
+        if (loggedIn) {
+          return this.ingestService.getUserAccount();
+        }
+        return of(undefined);
+      })
+    );
   }
 
   logout(e) {
     e.preventDefault();
     this.aai.logout();
   }
-
-  ngOnInit() {
-  }
-
 }
