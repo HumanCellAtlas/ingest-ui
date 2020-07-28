19bfbc44c1fc5c1e12258a9253fef73523141f88
Rolando Fernandez <rolando@ebi.ac.uk>
2020-07-28 09:51:19 +0100

    defined the exchange format with the spreadsheet generator service

--- client/src/app/template-questionnaire/template-questionnaire.data.ts
+++ client/src/app/template-questionnaire/template-questionnaire.data.ts
@@ -5,7 +5,20 @@ export interface QuestionnaireData {
   specimenType: string[];
 }
 
+interface TypeSpec {
+  schemaName: string;
+  excludeModules: string[];
+  excludeFields: string[];
+  embedProcess: boolean;
+  linkSpec: {
+    linkEntities: string[];
+    linkProtocols: string[]
+  };
+}
+
+
 export class TemplateSpecification {
+  types: TypeSpec[];
 
   static convert(data: QuestionnaireData): TemplateSpecification {
     return new TemplateSpecification();
